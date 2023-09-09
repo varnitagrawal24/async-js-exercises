6faa44638824c155f1c40493c81a289809f3aef4
Varnit Agrawal <varnit.agrawal24@gmail.com>
2023-09-09 10:56:52 +0530

    add append-file-promise.js

--- fs-promise/append-file-promise.js
+++ fs-promise/append-file-promise.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 const appendFilePromise = (filePath, content) => {
   return new Promise((resolve, reject) => {
     content += "\n";
-    fs.appendFile(filePath, content, (err, data) => {
+    fs.appendFile(filePath, content, (err) => {
       if (err) {
         reject(err);
       } else {
-        resolve(data);
+        resolve("content add successfully!!!");
       }
     });
   });
